501764316ffea5e5e778660bf768237a946cb6e8
Kalilo Hansmann <kalilohansmann@gmail.com>
2019-10-21 07:34:26 +0200

    Added tests for test_program.rb

---
+++ tests/test_program.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'program'
+
+class ProgramTest < Minitest::Test
+  def test_run_file
+    assert_raises { Program.new({ file: 'tests/bad_command.txt' }).run_file }
+    assert_raises { Program.new({ file: 'tests/bad_map_size.txt' }).run_file }
+    assert_raises { Program.new({ file: 'tests/bad_rover.txt' }).run_file }
+  end
+end
